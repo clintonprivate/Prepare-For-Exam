d83c37725659ea9d1df9a0d7873d2af4d456eb28
clintonprivate <152466284+clintonprivate@users.noreply.github.com>
2023-12-01 00:58:22 +0100

    Add files via upload

--- statementtoquestion.py
+++ statementtoquestion.py
@@ -1,11 +1,9 @@
 import spacy
 
-# Load the English language model
-nlp = spacy.load('en_core_web_sm')
+statement = 'a male cactus can have 5 different pricks'
 
-# Example sentence
-sentence = 'I ate a huge car.'
-doc = nlp(sentence)
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(statement)
 typeLabels = [
     'ROOT', 'acl', 'acomp', 'advcl', 'advmod', 'agent', 'amod', 'appos', 'attr', 
     'aux', 'auxpass', 'case', 'cc', 'ccomp', 'compound', 'conj', 'cop', 'csubj', 
@@ -13,8 +11,11 @@ typeLabels = [
     'npadvmod', 'nsubj', 'nsubjpass', 'nummod', 'oprd', 'parataxis', 'pcomp', 
     'pobj', 'poss', 'preconj', 'prep', 'prt', 'punct', 'quantmod', 'relcl', 'xcomp'
 ]
+questionWords = ["what", "who", "where", "when", "how many", "how"]
 questionTemplates = [
-    ["What", "auxpass", "subject", "ROOT"]
+    ["What", "auxpass", "det", "nsubjpass", "prep", "pobj", "ROOT"],
+    ["How many", "amod", "dobj", "aux", "det", "amod", "nsubj", "ROOT"],
+    ["nsubj", "ROOT", "what"],
 ]
 
 def extractSubject():
@@ -33,31 +34,61 @@ def extractObject():
             break
     return object
 
-def getWordByType(wordType):
+def getNextObject(position, template):
+    object = ""
+    objects = ["nsubj", "nsubjpass", "dobj"]
+    for index, part in enumerate(template):
+        if index > position and part in objects:
+            object = part
+            break
+    return object
+
+def getWordByType(wordType, nextObject):
+    findings = []
     for token in doc:
         if token.dep_ == wordType:
-            return token
+            findings.append(token)
+    for finding in findings:
+        if finding.head.dep_ == nextObject:
+            return finding.text
+    if len(findings) > 0:
+        return findings[0].text
+    return None
 
 def createQuestion():
     question = ""
     for template in questionTemplates:
-        for wordType in template:
-            try:
-                print(wordType)
-                if wordType not in typeLabels:
-                    question += wordType + " "
-                elif wordType == "subject":
-                    question += extractSubject() + " "
-                elif wordType == "object":
-                    question += extractObject() + " "
+        for index, wordType in enumerate(template):
+            if wordType in typeLabels:
+                found = getWordByType(wordType, getNextObject(index, template))
+                if found is not None:
+                    question += found + " "
                 else:
-                    found = getWordByType(wordType)
-                    if found is not None:
-                        question += found + " "
-                    else:
-                        continue
-            except:
-                pass
-    return question
+                    question = ""
+                    break
+            elif wordType.lower() in questionWords:
+                question += wordType + " "
+            elif wordType == "subject":
+                question += extractSubject() + " "
+            elif wordType == "object":
+                question += extractObject() + " "
+            else:
+                question = ""
+                break
+        question = question.strip()
+        if len(template) == len(question.split(" ")):
+            question += "?"
+            break
+    if question != "":
+        question += "?"
+        return question
+    return ""
 
+def listDependencies():
+    for token in doc:
+        print(token.text + " -> " + token.dep_ + " -> " + token.head.text + " -> " + token.head.dep_)
+    print()
+
+print()
 print(createQuestion())
+print()
